d3b4254120f6d77c28da568939c23516b7adf3ac
Marcel Lukas <m.lukas@mailbox.org>
2026-02-11 18:32:35 +0100

    feat: add django-cors-headers middleware

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -3,7 +3,6 @@ from .views import RegistrationView, CustomLoginView, LogoutView
 
 urlpatterns = [
   path('registration/', RegistrationView.as_view(), name='registration'),
-
   path('login/', CustomLoginView.as_view(), name='login'),
   path('logout/', LogoutView.as_view(), name='logout'),
 ]
