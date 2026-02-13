57b9d2d7d11ddaecdb3b90d49613ff2b8607f228
Marcel Lukas <m.lukas@mailbox.org>
2026-02-13 20:56:51 +0100

    refactor(board): streamline BoardSerializer and counts - Use owner_id source directly and clean up field formatting - Replace len(obj.members.all()) with obj.members.count() - Delegate create/update to ModelSerializer super() and set members separately

--- board_app/api/serializers.py
+++ board_app/api/serializers.py
@@ -4,51 +4,53 @@ from django.contrib.auth.models import User
 
 
 class BoardSerializer(serializers.ModelSerializer):
-        members = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, required=False, write_only=True)
-        owner_id = serializers.IntegerField(source='owner.id', read_only=True)
-        member_count = serializers.SerializerMethodField()
-        ticket_count = serializers.SerializerMethodField() 
-        tasks_to_do_count = serializers.SerializerMethodField()
-        tasks_high_prio_count = serializers.SerializerMethodField()
+    members = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        many=True,
+        required=False,
+        write_only=True
+    )
+    owner_id = serializers.IntegerField(source="owner_id", read_only=True)
 
-        class Meta:
-            model = Board
-            fields = [
-                 'id', 'title', 'members', 'member_count', 
-                 'ticket_count', 'tasks_to_do_count', 
-                 'tasks_high_prio_count', 'owner_id'
-            ]
-            read_only_fields = [
-                 'owner_id', 'member_count', 'ticket_count', 
-                 'tasks_to_do_count', 'tasks_high_prio_count'
-            ]
+    member_count = serializers.SerializerMethodField()
+    ticket_count = serializers.SerializerMethodField()
+    tasks_to_do_count = serializers.SerializerMethodField()
+    tasks_high_prio_count = serializers.SerializerMethodField()
 
-        def get_member_count(self, obj):
-            return len(obj.members.all())
-        
-        def get_ticket_count(self, obj):
-             return obj.tasks.count()
-        
-        def get_tasks_to_do_count(self, obj):
-             return obj.tasks.filter(status='to-do').count()
-        
-        def get_tasks_high_prio_count(self, obj):
-             return obj.tasks.filter(priority='high').count()
-        
-        def create(self, validated_data):
-             members = validated_data.pop('members', [])
-             validated_data.pop('owner', None)
-             board = Board.objects.create(owner=self.context['request'].user, **validated_data)
-             board.members.set(members)
-             return board
-        
-        def update(self, instance, validated_data):
-             members = validated_data.pop('members', None)
-             for attr, value in validated_data.items():
-                  setattr(instance, attr, value)
-             instance.save()
-             if members is not None:
-                  instance.members.set(members)
-             return instance
-        
+    class Meta:
+        model = Board
+        fields = [
+            "id", "title", "members", "member_count",
+            "ticket_count", "tasks_to_do_count",
+            "tasks_high_prio_count", "owner_id"
+        ]
+        read_only_fields = [
+            "owner_id", "member_count", "ticket_count",
+            "tasks_to_do_count", "tasks_high_prio_count"
+        ]
 
+    def get_member_count(self, obj):
+        return obj.members.count()
+
+    def get_ticket_count(self, obj):
+        return obj.tasks.count()
+
+    def get_tasks_to_do_count(self, obj):
+        return obj.tasks.filter(status="to-do").count()
+
+    def get_tasks_high_prio_count(self, obj):
+        return obj.tasks.filter(priority="high").count()
+
+    def create(self, validated_data):
+        members = validated_data.pop("members", [])
+        board = super().create(validated_data)
+        if members:
+            board.members.set(members)
+        return board
+
+    def update(self, instance, validated_data):
+        members = validated_data.pop("members", None)
+        instance = super().update(instance, validated_data)
+        if members is not None:
+            instance.members.set(members)
+        return instance
