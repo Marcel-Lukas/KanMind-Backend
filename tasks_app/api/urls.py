7d86c7f241bb4c767675ad437df83355eb225124
Marcel Lukas <m.lukas@mailbox.org>
2026-03-03 16:16:56 +0100

    feat(tasks): enhance CurrentUserTasksView for task filtering and assignment

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskListCreateView
+from .views import TaskListCreateView, CurrentUserTasksView
 
 urlpatterns = [
     path('tasks/', TaskListCreateView.as_view(), name='tasks'),
+    path('tasks/assigned-to-me/', CurrentUserTasksView.as_view(), name='tasks-assigned'),
+    path('tasks/reviewing/', CurrentUserTasksView().as_view(), name='tasks-reviewing'),
 ]
--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -17,3 +17,34 @@ class TaskListCreateView(generics.ListCreateAPIView):
             models.Q(board__members=user) | models.Q(board__owner=user)
         ).distinct()
 
+
+
+class CurrentUserTasksView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated] 
+
+    def get_queryset(self):
+        user = self.request.user
+        if 'assigned-to-me' in self.request.path:
+            return Task.objects.filter(
+                models.Q(board__members=user) | models.Q(board__owner=user), 
+                assignee=user,
+            ).distinct()
+        elif 'reviewing' in self.request.path:
+            return Task.objects.filter(
+                models.Q(board__members=user) | models.Q(board__owner=user),
+                reviewer=user,
+            ).distinct()
+        else:
+            return Task.objects.none()
+        
+    def perform_create(self, serializer):
+        if 'assigned-to-me' in self.request.path:
+            serializer.save(assignee=self.request.user)
+        elif 'reviewing' in self.request.path:
+            serializer.save(reviewer=self.request.user)
+        else:
+            serializer.save()
+
+
