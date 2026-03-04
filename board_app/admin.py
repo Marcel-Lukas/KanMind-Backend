c33922223ad3cb7068a0f91f456703c7c0d87b32
Marcel Lukas <m.lukas@mailbox.org>
2026-03-04 18:02:12 +0100

    feat(admin): register Task and TaskComment models with enhanced admin configurations

--- board_app/admin.py
+++ board_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Board
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'owner', 'member_count')
+	search_fields = ('title', 'owner__username', 'owner__email', 'members__username', 'members__email')
+	list_filter = ('owner',)
+	ordering = ('title',)
+	filter_horizontal = ('members',)
+	autocomplete_fields = ('owner',)
+
+	@admin.display(description='Members')
+	def member_count(self, obj):
+		return obj.members.count()
--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Task, TaskComment
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'title',
+		'board',
+		'status',
+		'priority',
+		'assignee',
+		'reviewer',
+		'due_date',
+		'comments_count',
+	)
+	search_fields = (
+		'title',
+		'description',
+		'board__title',
+		'assignee__username',
+		'assignee__email',
+		'reviewer__username',
+		'reviewer__email',
+	)
+	list_filter = ('status', 'priority', 'due_date', 'board')
+	ordering = ('due_date', '-id')
+	list_select_related = ('board', 'assignee', 'reviewer')
+	autocomplete_fields = ('board', 'assignee', 'reviewer')
+	date_hierarchy = 'due_date'
+
+
+@admin.register(TaskComment)
+class TaskCommentAdmin(admin.ModelAdmin):
+	list_display = ('id', 'task', 'author', 'created_at', 'short_content')
+	search_fields = ('content', 'author__username', 'author__email', 'task__title')
+	list_filter = ('created_at',)
+	ordering = ('-created_at',)
+	list_select_related = ('task', 'author')
+	autocomplete_fields = ('task', 'author')
+
+	@admin.display(description='Content')
+	def short_content(self, obj):
+		return obj.content[:60]
